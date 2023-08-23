645602b1b036c7b2b87117a9f4829d43fb3dccbc
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-08-23 23:25:24 +0300

    w

---
+++ src/js/cat-api.js
@@ -0,0 +1,7 @@
+import Notiflix from 'notiflix';
+
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_iaI6IZWu59cRc57tpwou70rQEt5HpMASN3XUcZegpYsQMmTHtXP9FKvQBc66myBx";
+
+const url = `https://api.thecatapi.com/v1/breeds`;
